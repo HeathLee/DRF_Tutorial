dc236139eaf5ca6fe1e842ba236f0a81f7e484e1
youcai.li <youcai.li@shanbay.com>
2016-08-16 18:35:50 +0800

    daily commit

--- snippets/models.py
+++ snippets/models.py
@@ -3,13 +3,13 @@ from pygments.lexers import get_all_lexers
 from pygments.styles import get_all_styles
 
 # Create your models here.
-LEXERS = [item for item in get_all_lexers if item[i]]
+LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
 class Snippet(models.Model):
-    created = models.DateTineField(auto_now_add=True)
-    title = models.CharField(max_length=100, bkank=True, default='')
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
     linenos = models.BooleanField(default=False)
     language = models.CharField(
@@ -22,4 +22,4 @@ class Snippet(models.Model):
         max_length=100)
 
     class Meta:
-        ordering = ('created')
+        ordering = ('created',)
