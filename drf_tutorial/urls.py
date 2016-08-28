64548eab02e18668934f07638bb8e58826d7296f
HeathLee <753940818@qq.com>
2016-08-28 21:51:34 +0800

    finish part 4

--- drf_tutorial/urls.py
+++ drf_tutorial/urls.py
@@ -15,6 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 
+
 urlpatterns = [
     url(r'^', include('snippets.urls')),
+]
+
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
